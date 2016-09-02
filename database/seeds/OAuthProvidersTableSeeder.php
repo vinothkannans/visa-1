d2791b9d5b6e0ad11cbf37eca17edf427ab015be
Vinoth Kannan <vinothkannan@vinkas.com>
2016-09-02 18:07:05 +0530

    db migrations & seeds for oauth providers & users

--- database/seeds/OAuthProvidersTableSeeder.php
+++ database/seeds/OAuthProvidersTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class OAuthProvidersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(OAuthProvidersTableSeeder::class);
+        DB::table('oauth_providers')->insert([
+            'name' => 'google',
+        ]);
     }
 }
