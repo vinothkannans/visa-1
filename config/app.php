c2faacfeb43aecfcb12ceb5bd083494af26570f4
Vinoth Kannan <vinothkannan@vinkas.com>
2016-09-06 01:16:07 +0530

    UPDATE: app config for socialite

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Laravel Service Providers...
          */
 
+        Laravel\Socialite\SocialiteServiceProvider::class,
         //
 
         /*
@@ -230,6 +231,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
