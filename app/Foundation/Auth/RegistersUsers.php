faf9f1f0f848c40580c9cfbd32829706bbd42452
Vinoth Kannan <vinothkannan@vinkas.com>
2016-09-06 01:34:32 +0530

    NEW: trait RegistersUsers

---
+++ app/Foundation/Auth/RegistersUsers.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Foundation\Auth;
+
+use Illuminate\Foundation\Auth\RegistersUsers as Registers;
+use Illuminate\Http\Request;
+
+trait RegistersUsers
+{
+  use Registers;
+
+  /**
+   * Show the application join form.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function showJoinForm()
+  {
+    return view('auth.join');
+  }
+
+  /**
+   * Handle a registration request for the application.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function join(Request $request)
+  {
+    $this->validator($request->all())->validate();
+    $this->guard()->login($this->create($request->all()));
+    return redirect($this->redirectPath());
+  }
+
+}
