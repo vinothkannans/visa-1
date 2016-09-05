9dad8a741e10b91c0051d563aa82bbe65494fa1f
Vinoth Kannan <vinothkannan@vinkas.com>
2016-09-06 01:31:43 +0530

    NEW: eloquent models

---
+++ app/OAuth/Provider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\OAuth;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Provider extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'oauth_providers';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
---
+++ app/OAuth/User.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\OAuth;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'oauth_users';
+
+    public $incrementing = false;
+}
